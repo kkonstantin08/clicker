f47c23dd62453ed2b3649d0d6248fb085f528efe
kkonstantin08 <kutasovkosta90@gmail.com>
2025-01-05 23:03:08 +0300

    .

--- src/main.js
+++ src/main.js
@@ -1,6 +1,18 @@
 // import './assets/main.css'
 
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
 
-createApp(App).mount('#app')
+createApp(App).mount("#app");
+
+function disableScroll() {
+  document.body.style.overflow = "hidden";
+}
+
+function enableScroll() {
+  document.body.style.overflow = "auto";
+}
+
+if (/Mobi|Android/i.test(navigator.userAgent)) {
+  disableScroll();
+}
